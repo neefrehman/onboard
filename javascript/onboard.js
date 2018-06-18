4a2e6cb2ecec88818cf227d546362386e6b898f2
Neef Rehman <neef.rehman@gmail.com>
2018-06-18 17:51:23 +0100

    :)

--- javascript/onboard.js
+++ javascript/onboard.js
@@ -4,14 +4,32 @@ const slack = require('./slack');
 const robin = require('./robin');
 const mailchimp = require('./mailchimp.js');
 
+
+$('#submit').click(function(){
+
+  var user_email = $('#EMAIL').val()
+  var first_name = $('#NAME').val()
+
+  main(user_email, first_name)
+  .then(() => console.log('success'))
+
+})
+
+$("#form").submit(function(){
+
+    window.open('https://groups.google.com/a/ustwo.com/forum/#!managemembers/floor.adv/add', '_blank');
+
+});
+
+
 function main(user_email, first_name){
+
   const proms = [sendgrid(user_email, first_name), slack(user_email, first_name), robin(user_email, first_name), mailchimp(user_email, first_name)];
   return Promise.all(proms)
   .then(() => console.log(`${user_email} added to all our services`));
+  
 }
 
-main('adlowe23@gmail.com', 'Adam')
-.then(() => console.log('success'))
 
 
 // // GET INPUT FROM #EMAIL and #NAME
